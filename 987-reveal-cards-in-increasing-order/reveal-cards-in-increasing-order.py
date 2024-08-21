5251cc638f32d61a8b13b9229f9c452695d767cd
Abduraheem-H <abdurahim.muzemil@aait.edu.et>
2024-08-21 18:29:25 +0300

    Time: 53 ms (12.81%) | Memory: 16.5 MB (98.66%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -4,9 +4,9 @@ class Solution:
         deck.sort(reverse=True)
         for i in range(len(deck)):
             if queue:
-                queue.append(queue.popleft())
-                queue.append(deck[i])
-            else:
-                queue.append(deck[i])
-        return [ i for i in reversed(queue)]
+                queue.appendleft(queue.pop())
+                
+        
+            queue.appendleft(deck[i])
+        return list(queue)
 #print(Solution.deckRevealedIncreasing(Solution, [17, 13, 11, 2, 3, 5, 7]))
