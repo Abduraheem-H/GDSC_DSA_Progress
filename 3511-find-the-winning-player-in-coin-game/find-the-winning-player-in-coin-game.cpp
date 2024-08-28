262be90a5fdc036c00a6a199281fef2b35742d99
Abduraheem-H <abdurahim.muzemil@aait.edu.et>
2024-08-28 23:28:24 +0300

    Time: 4 ms (28.06%) | Memory: 7.6 MB (42.76%) - LeetSync

---
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string losingPlayer(int x, int y) {
+        
+        int maxTurns = min(x, y / 4);
+        
+        if (maxTurns % 2 == 1) {
+            return "Alice";
+        } else {
+            return "Bob";
+        }
+    }
+};
