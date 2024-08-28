fe0bb705b4e135b183ad6cd23a5a05315235eefc
Abduraheem-H <abdurahim.muzemil@aait.edu.et>
2024-08-28 23:26:55 +0300

    Time: 35 ms (66.81%) | Memory: 16.5 MB (80.58%) - LeetSync

---
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def losingPlayer(self, x: int, y: int) -> str:
+        
+        total_turns = min(x, y // 4)
+        
+        
+        if total_turns % 2 == 1:
+            return "Alice"
+        else:
+            return "Bob"
